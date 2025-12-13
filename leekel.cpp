b783fe32c95658883b488a27d70886097dc56e9f
Liluo-Jax <574011891@qq.com>
2025-12-13 18:41:27 +0800

    更新项目文档
    然后没了

--- leekel.cpp
+++ leekel.cpp
@@ -1,28 +1,6 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-bool is_pal(const string &s, int l, int r){
-    while(l<r){
-        if(s[l]!=s[r]) return false;
-        l++; r--;
-    }
-    return true;
-}
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    string s;
-    if(!(cin>>s)) return 0;
-    int n = s.size();
-    int l = 0, r = n-1;
-    bool ok = true;
-    while(l<r){
-        if(s[l]==s[r]){ l++; r--; }
-        else{
-            if(is_pal(s, l+1, r) || is_pal(s, l, r-1)) ok = true;
-            else ok = false;
-            break;
-        }
-    }
-    cout << (ok?"YES":"NO");
+    printf("hi!leekel");
     return 0;
 }
